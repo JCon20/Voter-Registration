1fd1715ba3d386560ea57363c4d753b72ee27c8d
owShAd0w <149713690+owShAd0w@users.noreply.github.com>
2023-11-16 14:35:18 -0600

    Update RegistrarEmployee.h
    Still Connecting to Government, Half Done

--- RegistrarEmployee.h
+++ RegistrarEmployee.h
@@ -1,1 +1,39 @@
 #pragma once
+#include <iostream>
+
+using namespace std;
+
+class RegistrarEmployee {
+private:
+	string fName = "No First Name Recieved";
+	string lName = "No Last Name Recieved";
+	string email = "No Email Recieved";
+	string ssn = "No Social Security Number Recieved";
+	string address = "No Address Recieved";
+	string city = "No City Recieved";
+	string state = "No State Recieved";
+	string gender = "No Gender Recieved";
+
+public:
+	RegistrarEmployee();
+	~RegistrarEmployee();
+
+	string sendFName();
+	string sendLName();
+	string sendEmail();
+	string sendSSN();
+	string sendAddress();
+	string sendCity();
+	string sendState();
+	string sendGender();
+
+	void recieveFName(string newFName);
+	void recieveLName(string newLName);
+	void recieveEmail(string newEmail);
+	void recieveSSN(string newSSN);
+	void recieveAddress(string newAddress);
+	void recieveCity(string newCity);
+	void recieveState(string newState);
+	void recieveGender(string newGender);
+
+};
