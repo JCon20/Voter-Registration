7367d7f9771416e3a5f15f982c61910c66bd6916
kidlyd <149742781+kidlyd@users.noreply.github.com>
2023-11-16 22:10:13 -0600

    Create unitTest.cs

---
+++ unitTest.cs
@@ -0,0 +1,16 @@
+namespace unitTesting
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
