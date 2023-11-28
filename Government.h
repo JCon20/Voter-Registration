014f4992e6626645f37aa5a695adb90e8ffa13b6
nealanarmstrong <151072883+nealanarmstrong@users.noreply.github.com>
2023-11-28 11:44:09 -0600

    Update Government.h

--- Government.h
+++ Government.h
@@ -5,17 +5,17 @@ using namespace std;
 
 class Government {
 private:
-        string deptID = "No Department ID";
-        string deptName = "No Department Name";
-        string state = "No State";
+        string deptID = "10315";
+        string deptName = "Voter Verification Services";
+        string state = "Alabama";
 
 public:
         Government();
         ~Government();
 
-        string getDeptID();
+        /*string getDeptID();
         string getDeptName();
-        string getGovState();
-        string approveRequest();
-        string denyRequest();
+        string getGovState(); may or may not end up being used */
+        string approveRequest(); // approves the request for verification
+        string denyRequest(); // denies the request for verification
 };
