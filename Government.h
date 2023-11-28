e041c540c9bd4290816e97868bc2332223c25bd2
nealanarmstrong <151072883+nealanarmstrong@users.noreply.github.com>
2023-11-28 10:20:55 -0600

    updated government.h

--- Government.h
+++ Government.h
@@ -1,1 +1,21 @@
 #pragma once
+#include <iostream>
+
+using namespace std;
+
+class Government {
+private:
+        string deptID = "No Department ID";
+        string deptName = "No Department Name";
+        string state = "No State";
+
+public:
+        Government();
+        ~Government();
+
+        string getDeptID();
+        string getDeptName();
+        string getGovState();
+        string approveRequest();
+        string denyRequest();
+};
