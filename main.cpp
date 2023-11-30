037e94828e3b038d2264f8ebd05aab3319c0f32e
owShAd0w <149713690+owShAd0w@users.noreply.github.com>
2023-11-30 11:10:20 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,204 +1,127 @@
 #include <iostream>
 #include <stdlib.h>
+#include <conio.h>
+#include <limits>
 #include <string>
 
+#include "PersonList.h"
 #include "PersonInfo.h"
 #include "RegistrarEmployee.h"
-#include "Government.h"
-#include "VoterSubmission.h"
 
 using namespace std;
 
+void sendInfo();
+void recieveInfo(string fName, string lName, string email, string ssn, string address, string city, string state, string gender);
+void getUserInput(string fName, string lName, string email, string ssn, string address, string city, string state, string gender);
+
+void pressAnyKey();
+
 int main() {
+	PersonList PersonList;
 
-	int UIChoice1;
-	bool UIChoice2 = false;
-	
-	PersonInfo person;
-	RegistrarEmployee employee;
-	
+	pressAnyKey();
 
-	//Fills each string with empty data display.
-	string fName = person.getFName();
-	string lName = person.getLName();
-	string email = person.getEmail();
-	string ssn = person.getSSN();
-	string address = person.getAddress();
-	string city = person.getCity();
-	string state = person.getState();
-	string gender = person.getGender();
 
-	//Displays 'empty' values Eg. (No First Name)
-	/* Was used to verify system integrity. Commented out to clean up UI.
-	std::cout << fName << std::endl;
-	std::cout << lName << std::endl;
-	std::cout << email << std::endl;
-	std::cout << ssn << std::endl;
-	std::cout << address << std::endl;
-	std::cout << city << std::endl;
-	std::cout << state << std::endl;
-	std::cout << gender << std::endl;
-	*/
+	int choice = 0;
+	bool keepGoing = true;
+	while (keepGoing) {
+		cout << endl
+			<< "---------------------------------------------------------------------\n"
+			<< " Select an option from the menu below\n"
+			<< " by entering the number of the choice\n"
+			<< "---------------------------------------------------------------------\n"
+			<< "\t1\tAdd a new Person to the list\n"
+			<< "\t2\tCheck for existing Person\n"
+			<< "\t3\tChange data for existing Person\n"
+			<< "\t4\tDelete existing Person\n"
+			<< "\t5\tDisplay the list front to back\n"
+			<< "\t6\tEnd program\n"
+			<< "---------------------------------------------------------------------\n\n"
+			<< "Enter your choice : ";
 
-	//Begin User Interface
-	std::cout << "Thank you for choosing to participate in the voting process!\n";
-	
-	do {
-		std::cout << "Please select the option we can help you with today by entering the corresponding number.\n";
-		std::cout << "1. Register to Vote.\n2.Update existing voter information.\n";
-		std::getline(std::cin, UIChoice1);
+		cin >> choice;
 
-		switch(UIChoice1){
-			case 1: // UI for user to input and verify personal info.
-				std::cout << "Thank you for choosing to register to vote in the local Alabama election.\n";
-				std::cout << "First, we need to collect some of your information.\n";
-			
-				//Prompts user to modify and verify each value.
-				do {
-					person.modifyFName();
-					person.modifyLName();
-					person.modifyEmail();
-					person.modifySSN();
-					person.modifyAddress();
-					person.modifyCity();
-					person.modifyState();
-					person.modifyGender();
+		if (cin.fail()) {
+			cin.clear();
+			cin.ignore(std::numeric_limits<streamsize>::max(), '\n');
+			cout << "\n\tOnly integer input please!. \n";
+			pressAnyKey();
+			continue;
+		}
 
-					//Verify voter input.
-					std::cout << "Is the following information correct?\n";
-					std::cout << fName << std::endl;
-					std::cout << lName << std::endl;
-					std::cout << email << std::endl;
-					std::cout << ssn << std::endl;
-					std::cout << address << std::endl;
-					std::cout << city << std::endl;
-					std::cout << state << std::endl;
-					std::cout << gender << std::endl;
-					std::cout << "Please enter the number corresponding to your choice.\n0.No\n1.Yes\n";
-					std::getline(std::cin, UIChoice2);
-				} while (UIChoice2 == false);
-				
+		std::cin.ignore(std::numeric_limits<streamsize>::max(), '\n');
+		switch (choice) {
 
-				//Stores user input and displays final time.
-				std::cout << "The following information has been stored.\n";
-				fName = person.getFName();
-				lName = person.getLName();
-				email = person.getEmail();
-				ssn = person.getSSN();
-				address = person.getAddress();
-				city = person.getCity();
-				state = person.getState();
-				gender = person.getGender();
-				break;
+		case 1:
+		{
+			PersonInfo tempPerson;
+			PersonList.addPerson(tempPerson);
+			pressAnyKey();
+			break;
+		}
+		case 2:
+		{
+			string fName;
+			cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+			cout << "\nEnter the Person's First Name: ";
+			std::getline(std::cin, fName);
+			PersonList.findPerson(fName);
+			pressAnyKey();
+			break;
+		}
+		case 3:
+		{
+			string currentName;
+
+			cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+			cout << "\nEnter the first name of the Person you want to change: ";
+			std::getline(std::cin, currentName);
+			PersonList.editPerson(currentName);
+			pressAnyKey();
+			break;
+		}
+		case 4:
+		{
+			string fName;
+			cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+			cout << "\nEnter the first name of the person you want to delete: ";
+			std::getline(std::cin, fName);
+
+			PersonList.deletePerson(fName);
+			pressAnyKey();
+			break;
+		}
+		case 5:
+		{
+			PersonList.displayList();
+			pressAnyKey();
+			break;
+		}
 		
-			case 2: // UI for user to modify existing voter info.
-				std::cout << "This decision hasn't been fully implemented."; // I'm unsure how to check user input to see if that voter name exists. Can someone assist? 
+		case 6:
+		{
+			std::cout << "The program is now ending." << std::endl;
+			keepGoing = false;
+			pressAnyKey();
+			break;
+		}
+		default:
+		{
+			cout << "That option is currently not available\n";
+			pressAnyKey();
+			break;
+		}
+		}
+	}
 
-				/*
-				In previous code, I had:
-    
-    string fullNameCheck;
-    int UIChoice3;
-    int UIChoice4 = 1;
-    string activeAccount;
-  
-    cout << "\nWhat is the name of the account you'd like to update: \n";
-    getline(cin, fullNameCheck);
-  do{
-    if (fullNameCheck == fullName){
-      cout << "\nWhich data would you like to change?\n1. " << first_name << "\n2. " << last_name << "\n3. ";
-      cout << email << "\n4. " << SSN << "\n5. " << gender << "\n6. " << street_address << "\n7. " << city; 
-      cout << "\n8. " << state << "\n9. " << zipCode << "\n";
-    }else{
-      cout << "That name is not in our system. Please try another name.\n";
-      updateInfo();
-      };
-    cin >> UIChoice3;
-    cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-  switch(UIChoice3){
-    case 1:
-        cout << "Please enter your first name: \n";
-        getline(cin, first_name);
-        activeAccount = first_name + " " + last_name;
-        cout << "Your information has been updated\n";
-        break;
-    case 2:
-        cout << "Please enter your last name: \n";
-        getline(cin, last_name);
-        activeAccount = first_name + " " + last_name;
-        cout << "Your information has been updated\n";
-        break;
-    case 3:
-        cout << "Please enter your email: \n";
-        getline(cin, email);
-        cout << "Your information has been updated\n";
-        break;
-    case 4:
-        cout << "Please enter your SSN: \n";
-        getline(cin, SSN);
-        cout << "Your information has been updated\n";
-        break;
-    case 5:
-        cout << "Please enter your gender: \n";
-        getline(cin, gender);
-        cout << "Your information has been updated\n";
-        break;
-    case 6:
-        cout << "Please enter your street address: \n";
-        getline(cin, street_address);
-        cout << "Your information has been updated\n";
-        break;
-    case 7:
-        cout << "Please enter your city: \n";
-        getline(cin, city);
-        cout << "Your information has been updated\n";
-        break;
-    case 8:
-        cout << "Please enter your state: \n";
-        getline(cin, state);
-        cout << "Your information has been updated\n";
-        break;
-    case 9:
-        cout << "Please enter your zip code: \n";
-        getline(cin, zipCode);
-        cout << "Your information has been updated\n";
-        break;
-    default:
-        cout << "That is not a valid option. Please try again.\n";
-        updateInfo();
-        break;
-  }
-  cout << "\nWould you like to update another piece of information?\n1. Yes\n2. No\n";
-  cin >> UIChoice4;
-  }while(UIChoice4 == 1);
-  fullName = activeAccount;
-  }
-				*/
-				break;
-		
-			default:
-				std::cout << "This is not a valid option. Please type the number corresponding to your choice. (1 or 2)\n";
-		} while (UIChoice1 < 1 || UIChoice1 > 2);
-			
-			
-
+	return 0;
 	
-	employee.recieveFName(fName);
-	employee.recieveLName(lName);
-	employee.recieveEmail(email);
-	employee.recieveSSN(ssn);
-	employee.recieveAddress(address);
-	employee.recieveCity(city);
-	employee.recieveState(state);
-	employee.recieveGender(gender);
+}
 
-	employee.sendFName();
-	employee.sendLName();
-	employee.sendEmail();
-	employee.sendSSN();
-	employee.sendAddress();
-	employee.sendCity();
-	employee.sendState();
-	employee.sendGender();
-
+void pressAnyKey() {
+	char ch = '\0';
+	cout << "Press any key to continue" << endl << endl;
+	ch = _getch();
+	system("cls");
+	return;
 }
