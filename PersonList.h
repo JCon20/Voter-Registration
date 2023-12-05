2ba0bd898a9676774f48eef55a7cf863ecb2c0f0
owShAd0w <149713690+owShAd0w@users.noreply.github.com>
2023-12-05 16:28:09 -0600

    Update PersonList.h

--- PersonList.h
+++ PersonList.h
@@ -5,21 +5,24 @@
 using namespace std;
 class PersonList {
 
-private :
+private:
 	PersonNode* head;
 	PersonNode* tail;
 
 
-	public:
-		PersonList();
-		~PersonList();
+public:
+	PersonList();
+	~PersonList();
 
-		bool empty();
-		void addPerson(PersonInfo person);
-		bool findPerson(string ssn);
-		void editPerson(const string& currenSSN);
-		void deletePerson(const string& ssn);
-		void removeFront();
-		void displayList();
-		void displayVoter(string ssn);
+	string getCity(int ssn);
+	string getState(int ssn);
+
+	bool empty();
+	void addPerson(PersonInfo person);
+	bool findPerson(int ssn);
+	void editPerson(const int& currenSSN);
+	void deletePerson(const int& ssn);
+	void removeFront();
+	void displayList();
+	void displayPerson(int ssn);
 };
