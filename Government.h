de12d2c499292225097f78e41277a49a568e6bb1
owShAd0w <149713690+owShAd0w@users.noreply.github.com>
2023-12-05 16:26:39 -0600

    Update Government.h

--- Government.h
+++ Government.h
@@ -5,22 +5,24 @@ using namespace std;
 
 class Government {
 private:
-        string deptID = "10315";
-        string deptName = "Voter Verification Services";
-        string state = "Alabama";
-        string county = "Mobile County";
-        string city = "Mobile";
+    string deptID = "10315";
+    string deptName = "Voter Verification Services";
+    string state = "Alabama";
+    string county = "Mobile County";
+    string city = "Mobile";
+
+    bool approved;
+    bool registered;
 
 public:
-        Government();
-        ~Government();
+    Government();
+    ~Government();
 
-        bool valid_city();
-        bool valid_state();
-        bool valid_zipcode();
-        bool eligibilityCheck();
-        void approveRegistration(); // approves the request for verification
-        void checkVoterDatabase();
-        void updateVoterDatabase();
-        
+    bool valid_city(int ssn);
+    bool valid_state(int ssn);
+    bool eligibilityCheck(int ssn);
+    void approveRegistration(int ssn); // approves the request for verification
+    bool checkVoterDatabase(int ssn);
+    void updateVoterDatabase();
+
 };
